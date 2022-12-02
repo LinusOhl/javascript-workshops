665838fbc50594d970fc43c4375b9052711edb15
Linuss <tade3d@gmail.com>
2022-12-02 10:32:07 +0100

    .

--- 06-xmlhttprequest/script.js
+++ 06-xmlhttprequest/script.js
@@ -1,26 +1,66 @@
 /**
- * XMLHttpRequest
+ * Async pets
  *
  */
 
-const app = document.querySelector("#app");
-const request = new XMLHttpRequest();
+const getJSON = (url, callback) => {
+  // Create a new XML Http Request
+  const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4 && request.status === 200) {
-    let todos = JSON.parse(request.responseText);
+  // Attach an event-listener to the request
+  request.addEventListener("readystatechange", () => {
+    // Check if readyState is 4 (=== DONE)
+    if (request.readyState === 4) {
+      // Was the request successful (200 = OK)?
+      if (request.status === 200) {
+        // Request was 200 OK!
 
-    todos.forEach((todo) => {
-      app.innerHTML += `
-        <div class="card mb-2">
-          <div class="card-body">
-            ${todo.title}
-          </div>
-        </div>
-      `;
+        // take the STRING in responseText and PARSE it into JavaScript object
+        const data = JSON.parse(request.responseText);
+
+        // invoke(call) callback
+        callback(undefined, data);
+      } else {
+        // Something went wrong :'(
+        console.log("ERROR ERROR DANGER WILL ROBINSON!");
+
+        // invoke(call) callback
+        callback("Could not get data");
+      }
+    }
+  });
+
+  request.open("GET", url); // Set request to GET data
+  request.send(); // Send the request
+  console.log("Request sent!");
+};
+
+getJSON("data/cats.json", (err, cats) => {
+  if (err) {
+    alert(err);
+  } else {
+    cats.forEach((cat) => {
+      document.querySelector("#cats").innerHTML += `<li>${cat.name}</li>`;
+    });
+    getJSON("data/dogs.json", (err, dogs) => {
+      if (err) {
+        alert(err);
+      } else {
+        dogs.forEach((dog) => {
+          document.querySelector("#dogs").innerHTML += `<li>${dog.name}</li>`;
+        });
+        getJSON("data/birds.json", (err, birds) => {
+          if (err) {
+            alert(err);
+          } else {
+            birds.forEach((bird) => {
+              document.querySelector(
+                "#birds"
+              ).innerHTML += `<li>${bird.name}</li>`;
+            });
+          }
+        });
+      }
     });
   }
 });
-
-request.open("GET", "https://jsonplaceholder.typicode.com/todos");
-request.send();
---
+++ 07-weather-app/assets/js/app.js
@@ -0,0 +1,90 @@
+/**
+ * 🌧️.
+ *
+ */
+
+const renderAlert = (msg, type = "info") => {
+  document.querySelector("#forecast").innerHTML = `
+    <div class="alert alert-${type}">${msg}</div>
+  `;
+};
+
+const renderNotice = (msg) => renderAlert(msg, "info");
+const renderWarning = (msg) => renderAlert(msg, "warning");
+const renderError = (msg) => renderAlert(msg, "danger");
+
+const renderCurrentWeather = (data) => {
+  const conditions = data.weather.map(
+    (condition) =>
+      `
+        <li>
+          <img src="http://openweathermap.org/img/wn/${condition.icon}@2x.png" title="${condition.description}">
+        </li>
+      `
+  );
+
+  const now = Math.round(Date.now() / 1000);
+  const banner =
+    now > data.sys.sunrise && now < data.sys.sunset
+      ? "assets/images/day.svg"
+      : "assets/images/night.svg";
+
+  const freshness = new Date(data.dt * 1000);
+
+  document.querySelector("#forecast").innerHTML = `
+		<div class="card">
+			<img src="${banner}" class="card-img-top">
+			<div class="card-body">
+				<h5 class="card-title" id="location">
+					<span id="city">${data.name}</span>,
+					<span id="country">${data.sys.country}</span>
+				</h5>
+				<p class="temp">
+					<span id="temperature">${data.main.temp}</span>
+					&deg;C
+				</p>
+				<p class="humidity">
+					<span id="humidity">${data.main.humidity}</span>
+					&percnt; humidity
+				</p>
+				<p class="wind">
+					<span id="windspeed">${data.wind.speed}</span>
+					m/s
+				</p>
+        <ul class="conditions">
+          ${conditions.join("")}
+        </ul>
+
+        <p class="text-muted small">${freshness.toLocaleString()}</p>
+			</div>
+		</div>
+	`;
+};
+
+document.querySelector("#search-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  document.querySelector("#forecast").innerHTML = `
+    <div class="spinner-border text-light" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  `;
+
+  const city = e.target.query.value.trim();
+
+  if (city.length < 3) {
+    renderNotice("Please enter at least 3 chars");
+    return;
+  }
+
+  // do search
+  console.log(`Searching for city "${city}"`);
+
+  try {
+    const data = await getCurrentWeather(city);
+    // render current weather conditions
+    renderCurrentWeather(data);
+  } catch (e) {
+    renderWarning("City not found!");
+  }
+});
