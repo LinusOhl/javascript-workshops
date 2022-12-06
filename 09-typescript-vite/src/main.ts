c58147d4cf9cf7a34132144f7809d0e430155023
Linuss <tade3d@gmail.com>
2022-12-06 15:21:09 +0100

    .

--- 09-typescript-vite/src/main.ts
+++ 09-typescript-vite/src/main.ts
@@ -1,9 +1,23 @@
 import "./style.css";
 
 const newTodoForm = document.querySelector("#new-todo-form");
+const todosList = document.querySelector("#todos")!;
+
+const getRandomNumber = (max: number = 10) => {
+  return Math.ceil(Math.random() * max);
+};
+console.log(getRandomNumber(10));
 
 newTodoForm?.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  console.log("test");
+  const inputNewTodoTitle =
+    document.querySelector<HTMLInputElement>("#new-todo-title")!;
+  const newTodoTitle = inputNewTodoTitle.value;
+
+  todosList.innerHTML += `
+    <li>${newTodoTitle}</li>
+  `;
+
+  console.log(newTodoTitle);
 });
